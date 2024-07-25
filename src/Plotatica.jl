fc29305c2efa20d0dd9500c626aba51e14517778
nondairyneutrino <nathaniel.chapman@cwu.edu>
2024-07-24 18:06:08 -0700

    add plot

--- src/Plotatica.jl
+++ src/Plotatica.jl
@@ -3,4 +3,19 @@ module Plotatica
 export plot
 using MathLink
 
+# define plotting to be consistent with other data-based numerical plotting
+"""
+    plot(data :: Matrix{T}) :: MathLink.WExpr where T <: Number
+
+TBW
+"""
+function plot(data :: Matrix{T}) :: MathLink.WExpr where T <: Number
+    expr = W`ListLinePlot[
+        $data,
+        Background -> White,
+        ImagePadding -> 15
+    ]`
+    return weval(expr)
+end
+
 end
